618a5080ce259b44bfc70b5c012478284fd55738
Pedrocm99 <pedrocasasdev@gmail.com>
2025-01-25 12:00:26 +0100

    Project setup 2
    Glad.c, Basic window and openGL context

--- BarebonesProject/BarebonesProject/main.cpp
+++ BarebonesProject/BarebonesProject/main.cpp
@@ -14,6 +14,46 @@
 
 int main()
 {
-	std::cout << "Hello World!" << std::endl;
+	glfwInit();
+
+	// Require OpenGL 3.3:
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+	// Explicitly use the core-profile (no backwards compatible features required)
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Mac Initialization:
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+	// CREATE WINDOW:
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// GLAD: LOAD ALL OPENGL FUNCTION POINTERS
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
 }
