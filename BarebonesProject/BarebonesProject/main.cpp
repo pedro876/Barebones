b897e699981e7921b7ef25ccfb8d9de2506e9e44
Pedrocm99 <pedrocasasdev@gmail.com>
2025-01-25 11:54:10 +0100

    Project setup

---
+++ BarebonesProject/BarebonesProject/main.cpp
@@ -0,0 +1,19 @@
+//STD INCLUDES:
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <cassert>
+
+// OPEN GLAND THIRD PARTY INCLUDES:
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include "stb_image.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+int main()
+{
+	std::cout << "Hello World!" << std::endl;
+	return 0;
+}
