72290181bfebc535fe020940f8ce1867eea5ca90
Pedrocm99 <pedrocasasdev@gmail.com>
2025-03-18 18:30:20 +0100

    Light struct glsl

--- BarebonesProject/BarebonesProject/Core/Assets/ShaderLibrary/Common.glsl
+++ BarebonesProject/BarebonesProject/Core/Assets/ShaderLibrary/Common.glsl
@@ -1,9 +1,15 @@
-uniform mat4 _Model;
+// MACROS
+#define saturate(x) clamp(x, 0.0, 1.0)
 
+// FUNCTIONS
+float Square(float x) { return x * x; }
+vec2 Square(vec2 x) { return x * x; }
+vec3 Square(vec3 x) { return x * x; }
+vec4 Square(vec4 x) { return x * x; }
+
+// INPUTS
+uniform mat4 _Model;
 layout (std140) uniform Matrices
 {
     mat4 _ViewProj;
 };
-
-#define saturate(x) clamp(x, 0.0, 1.0)
-#define pow2(x) (x*x)
--- BarebonesProject/BarebonesProject/Core/Assets/ShaderLibrary/Lighting.glsl
+++ BarebonesProject/BarebonesProject/Core/Assets/ShaderLibrary/Lighting.glsl
@@ -1,21 +1,34 @@
 
 uniform vec3 _AmbientLight = vec3(0.1);
 
+struct Light
+{
+	vec3 positionWS;
+	float invSqrRange;
+	float intensity;
+};
+
+float DistanceAttenuation(Light light, float distanceSqr, vec3 positionWS)
+{
+	return Square(saturate(1.0 - Square(distanceSqr * light.invSqrRange)));
+}
+
 vec3 GetLighting(vec3 positionWS, vec3 normalWS)
 {
 	vec3 totalLight = _AmbientLight;
 
-	vec3 lightPos = vec3(0.0, 2.0, 1.0);
-	float lightRangeSqr = pow2(4.0);
-	float lightIntensity = 2.0;
+	Light light;
+	light.positionWS = vec3(0.0, 2.0, 1.0);
+	light.intensity = 2.0;
+	light.invSqrRange = 1.0 / Square(4.0);
 
-	vec3 lightDir = lightPos - positionWS;
+	vec3 lightDir = light.positionWS - positionWS;
 	float distSqr = dot(lightDir, lightDir);
 	lightDir = normalize(lightDir);
-	float atten = distSqr / lightRangeSqr;
-	atten = 1.0 - saturate(atten);
+
+	float atten = DistanceAttenuation(light, distSqr, positionWS);
 	atten *= saturate(dot(lightDir, normalWS));
-	atten *= lightIntensity;
+	atten *= light.intensity;
 
 	totalLight += vec3(atten);
 
